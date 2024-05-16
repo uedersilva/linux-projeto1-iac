3fd099a0ca4751c9b8b861710550af67219f0c5e
uedersilva <uedersilva@gmail.com>
2024-05-16 18:25:45 -0300

    Update iac1.sh

--- iac1.sh
+++ iac1.sh
@@ -15,17 +15,17 @@ groupadd GRP_SEC
 
 echo "Criando usuários..."
 
-useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd tania -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
 useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd lucio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
 
-useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
-useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
-useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd cloves -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd jennifer -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd caroline -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
 
-useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
-useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
-useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd carine -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd kelli -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd vanessa -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 
 echo "Especificando permissões dos diretórios...."
 
